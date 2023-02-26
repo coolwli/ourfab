a53ef611f787c32dcfc56c3daabed978fda7e1b8
coolwli <lcndra03@gmail.com>
2023-02-26 15:45:36 +0300

    assets added

---
+++ Assets/packages/Character Movement Fundamentals/Source/Scripts/Input/Character/CharacterKeyboardInput.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Threading.Tasks;
+namespace CMF
+{
+    public class CharacterKeyboardInput : CharacterInput
+    {
+	    [Header("Scripts")]
+	    public FloatingJoystick floatingJoystick;
+
+	    public bool jump;
+
+        public override float GetHorizontalMovementInput()
+        {
+	        return floatingJoystick.Horizontal;
+	        
+        }
+
+		public override float GetVerticalMovementInput()
+		{ 
+			return floatingJoystick.Vertical;
+		}
+
+		public override bool IsJumpKeyPressed()
+		{
+			return jump;
+		}
+
+		public async void jumpButton()
+		{
+			jump = true;
+			await Task.Delay(1180);
+			jump = false;
+
+		}
+    }
+}
